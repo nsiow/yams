f38d5a4562780abf7b8f7461366ae5a5015a583b
nsiow <siow4096@gmail.com>
2024-09-01 11:38:04 -0700

    Add caching; fix data type bugs

--- misc/orgcrawl.py
+++ misc/orgcrawl.py
@@ -1,14 +1,30 @@
 #!/usr/bin/env python3
 
 import json
+import logging
+import os
+import sys
 from typing import Dict, List
 
 import boto3
-from botocore.client import BaseClient
+import joblib
 
-def get_org_root(client: BaseClient) -> str:
+# Set up logging
+logging.basicConfig(level=os.environ.get('YAMS_LOG_LEVEL', 'INFO').upper(),
+                    stream=sys.stdout)
+for name in ['boto', 'botocore', 'urllib3', 's3transfer']:
+    logging.getLogger(name).setLevel(logging.WARNING)
+
+# Set up cache
+mem = joblib.Memory('/tmp/orgcrawl.cache')
+
+# Set up client
+orgclient = boto3.client('organizations')
+
+@mem.cache
+def get_org_root() -> str:
     """Returns the ID of the organization root."""
-    resp = client.list_roots()
+    resp = orgclient.list_roots()
     roots = resp['Roots']
 
     if len(roots) != 1:
@@ -16,7 +32,8 @@ def get_org_root(client: BaseClient) -> str:
 
     return roots[0]['Id']
 
-def get_org_structure(client: BaseClient, org_root_id: str) -> Dict:
+@mem.cache
+def get_org_structure(org_root_id: str) -> Dict:
     """Recurse through the org structure and return a representation of the root/ou/acct hierarchy.
 
     At the end of evaluation, `structure` contains a map of accounts to their position within the
@@ -56,7 +73,7 @@ def get_org_structure(client: BaseClient, org_root_id: str) -> Dict:
 
         # Otherwise list children and add them to the queue
         for type_ in ['ORGANIZATIONAL_UNIT', 'ACCOUNT']:
-            resp = client.list_children(ParentId=base, ChildType=type_)
+            resp = orgclient.list_children(ParentId=base, ChildType=type_)
             children = resp['Children']
             for child in children:
                 id_ = child['Id']
@@ -65,7 +82,8 @@ def get_org_structure(client: BaseClient, org_root_id: str) -> Dict:
 
     return structure
 
-def get_policy_structure(client: BaseClient, org_structure: Dict) -> Dict:
+@mem.cache
+def get_policy_structure(org_structure: Dict) -> Dict:
     """Traverse the org structure and for each node, look up the attached policies.
 
     At the end of evaluation, `structure` contains a map of org entities to the policies attached
@@ -90,17 +108,18 @@ def get_policy_structure(client: BaseClient, org_structure: Dict) -> Dict:
     for target_list in org_structure.values():
         for target_id in target_list:
             if target_id not in structure:
-                resp = client.list_policies_for_target(TargetId=target_id,
+                resp = orgclient.list_policies_for_target(TargetId=target_id,
                                                        Filter='SERVICE_CONTROL_POLICY')
                 structure[target_id] = [p['Id'] for p in resp['Policies']]
 
     return structure
 
-def get_policies(client: BaseClient, policy_structure: Dict) -> Dict:
+@mem.cache
+def get_policies(policy_structure: Dict) -> Dict:
     """Traverse the attached policies and describe each of them.
 
     At the end of evaluation, `policies` contains a map of policy IDs to definitions
-    to that entity, e.g.
+    of that entity, e.g.
 
     {
       "p-12345": {
@@ -121,7 +140,7 @@ def get_policies(client: BaseClient, policy_structure: Dict) -> Dict:
     for policy_list in policy_structure.values():
         for policy_id in policy_list:
             if policy_id not in policies:
-                resp = client.describe_policy(PolicyId=policy_id)
+                resp = orgclient.describe_policy(PolicyId=policy_id)
                 policies[policy_id] = resp['Policy']
 
     return policies
@@ -130,7 +149,7 @@ def as_config_blobs(org_structure: Dict, policy_structure: Dict, policy_data: Di
     """Combine the org + policy structure into a parsable format."""
     data = []
 
-    for account, ou_path in org_structure.values():
+    for account, ou_path in org_structure.items():
         policies = []
         for node in ou_path:
             node_policies = []
@@ -139,27 +158,44 @@ def as_config_blobs(org_structure: Dict, policy_structure: Dict, policy_data: Di
                 node_policies.append(policy)
             policies.append(node_policies)
 
+        parsed_policies = []
+        for policy_level in policies:
+            parsed_policy_level = []
+            for policy_obj in policy_level:
+                policy_summary = policy_obj['PolicySummary']
+                policy_content = policy_obj['Content']
+                policy = json.loads(policy_content)
+                policy['PolicyId'] = get_policy_id(policy_summary)
+                parsed_policy_level.append(policy)
+            parsed_policies.append(parsed_policy_level)
+
         account_config_blob = {
-            "arn": f"yams/{account}",
-            "resourceType": "Yams::Organizations::Account",
-            "accountId": account,
-            "configuration": {
-              "ServiceControlPolicies": policies,
+            'arn': f'arn:yams:::account/{account}',
+            'resourceType': 'Yams::Organizations::Account',
+            'accountId': account,
+            'configuration': {
+              'serviceControlPolicies': parsed_policies,
             },
         }
         data.append(account_config_blob)
 
     return data
 
+def get_policy_id(policy_summary: Dict) -> str:
+    """Return a useful, human-readable identifier for the provided policy."""
+    return f'{policy_summary["Arn"]}/{policy_summary["Name"]}'
+
 def main():
-    client = boto3.client('organizations')
-    org_root_id = get_org_root(client)
+    org_root_id = get_org_root()
     print(f'[✓] Discovered root: {org_root_id}')
-    org_structure = get_org_structure(client, org_root_id)
+    org_structure = get_org_structure(org_root_id)
+    logging.debug('org structure: %s', org_structure)
     print(f'[✓] Discovered org structure for {len(org_structure)} entities')
-    policy_structure = get_policy_structure(client, org_structure)
+    policy_structure = get_policy_structure(org_structure)
+    logging.debug('policy structure: %s', policy_structure)
     print(f'[✓] Discovered policy structure for {len(policy_structure)} entities')
-    policy_data = get_policies(client, policy_structure)
+    policy_data = get_policies(policy_structure)
+    logging.debug('policy data: %s', policy_data)
     print(f'[✓] Discovered details for {len(policy_data)} policies')
     data = as_config_blobs(org_structure, policy_structure, policy_data)
     with open('orgdump.json', 'w+') as f:
