ee5f3f225cd3cb86cf7ecbb5aa79947f5dd8e12a
nsiow <siow4096@gmail.com>
2025-04-01 20:39:49 -0700

    Finish implementation of sar_v2

--- misc/sar_v2.py
+++ misc/sar_v2.py
@@ -4,6 +4,7 @@ import gzip
 import json
 import logging
 import os
+import re
 import sys
 
 import joblib
@@ -29,8 +30,8 @@ class ResourcePointer(pydantic.BaseModel):
 
 class Action(pydantic.BaseModel):
     Name: str
-    ActionConditionKeys: list[str]
-    Resources: list[ResourcePointer]
+    ActionConditionKeys: list[str] = []
+    Resources: list[ResourcePointer] = []
 
 class Condition(pydantic.BaseModel):
     Name: str
@@ -38,15 +39,15 @@ class Condition(pydantic.BaseModel):
 
 class Resource(pydantic.BaseModel):
     Name: str
-    ARNFormats: list[str]
-    ConditionKeys: list[str]
+    ARNFormats: list[str] = []
+    ConditionKeys: list[str] = []
 
 class Service(pydantic.BaseModel):
     Name: str
     Version: str
     Actions: list[Action]
-    ConditionKeys: list[Condition]
-    Resources: list[Resource]
+    ConditionKeys: list[Condition] = []
+    Resources: list[Resource] = []
 
 # ------------------------------------------------------------------------------------------------
 # SETUP
@@ -58,25 +59,34 @@ logging.basicConfig(level=os.environ.get('YAMS_LOG_LEVEL', 'INFO').upper(),
 
 # Set up cache
 os.makedirs('.cache', exist_ok=True)
-memory = joblib.Memory('.cache/sar_v2.cache')
+mem = joblib.Memory('.cache/sar_v2.cache')
 
 # ------------------------------------------------------------------------------------------------
 # HELPERS
 # ------------------------------------------------------------------------------------------------
 
+@mem.cache
 def fetch_service_listing() -> list[ServiceListing]:
     resp = requests.get(SERVICE_REFERENCE_URL)
     resp.raise_for_status()
     return [ServiceListing(**s) for s in resp.json()]
 
+@mem.cache
 def fetch_service(service_listing: ServiceListing) -> Service:
     resp = requests.get(service_listing.url)
     resp.raise_for_status()
     return Service(**resp.json())
 
-def restructure(services: list[Service]) -> dict:
-    # TODO(nsiow) 
-    return {}
+def normalize(service: Service) -> Service:
+    service = normalize_condition_variables(service)
+    return service
+
+def normalize_condition_variables(service: Service) -> Service:
+    for action in service.Actions:
+        for i in range(len(action.ActionConditionKeys)):
+            condition_key = re.sub(r'[/:]\${[a-zA-Z0-9]+}$', '', action.ActionConditionKeys[i])
+            action.ActionConditionKeys[i] = condition_key
+    return service
 
 # ------------------------------------------------------------------------------------------------
 # MAIN
@@ -84,15 +94,15 @@ def restructure(services: list[Service]) -> dict:
 
 def main():
     service_listing = fetch_service_listing()
-    services = [fetch_service(s) for s in service_listing]
-    sar_data = restructure(services)
+    services = [normalize(fetch_service(s)) for s in service_listing]
+    services_json = [s.model_dump(exclude_defaults=True) for s in services]
 
     if len(sys.argv) >= 2:
         out = gzip.open(sys.argv[1], 'wt')
     else:
         out = sys.stdout
 
-    json.dump(sar_data, out)
+    json.dump(services_json, out)
     out.close()
 
 if __name__ == '__main__':
