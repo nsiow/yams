0789c99c34f8fe9a9339476baa766cd809ff2c9f
nsiow <siow4096@gmail.com>
2025-04-14 22:19:45 -0700

    Finish builder and move to separate file

--- pkg/entities/universe.go
+++ pkg/entities/universe.go
@@ -27,61 +27,6 @@ func NewUniverse() *Universe {
 }
 
 // -------------------------------------------------------------------------------------------------
-// Builder
-// -------------------------------------------------------------------------------------------------
-
-// UniverseBuilder provides convenience functions for constructing universes using the builder
-// pattern
-type UniverseBuilder struct {
-	universe *Universe
-}
-
-// NewUniverse creates and returns a new, empty universe
-func NewBuilder() *UniverseBuilder {
-	return &UniverseBuilder{universe: NewUniverse()}
-}
-
-// WithAccounts adds the provided accounts to the universe under construction
-func (ub *UniverseBuilder) WithAccounts(accounts ...Account) *UniverseBuilder {
-	for _, a := range accounts {
-		ub.universe.PutAccount(a)
-	}
-	return ub
-}
-
-// WithGroups adds the provided groups to the universe under construction
-func (ub *UniverseBuilder) WithGroups(groups ...Group) *UniverseBuilder {
-	for _, g := range groups {
-		ub.universe.PutGroup(g)
-	}
-	return ub
-}
-
-// WithPolicies adds the provided policies to the universe under construction
-func (ub *UniverseBuilder) WithPolicies(policies ...Policy) *UniverseBuilder {
-	for _, p := range policies {
-		ub.universe.PutPolicy(p)
-	}
-	return ub
-}
-
-// WithPrincipals adds the provided principals to the universe under construction
-func (ub *UniverseBuilder) WithPrincipals(principals ...Principal) *UniverseBuilder {
-	for _, p := range principals {
-		ub.universe.PutPrincipal(p)
-	}
-	return ub
-}
-
-// WithResources adds the provided resources to the universe under construction
-func (ub *UniverseBuilder) WithResources(resources ...Resource) *UniverseBuilder {
-	for _, r := range resources {
-		ub.universe.PutResource(r)
-	}
-	return ub
-}
-
-// -------------------------------------------------------------------------------------------------
 // Accounts
 // -------------------------------------------------------------------------------------------------
 
