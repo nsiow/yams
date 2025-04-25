aba6b3854b9d866a228a93e175f022b3753afb00
nsiow <siow4096@gmail.com>
2025-04-24 21:47:27 -0700

    Working trace reimplmentation + test

--- pkg/sim/trace/trace.go
+++ pkg/sim/trace/trace.go
@@ -1,27 +1,38 @@
 package trace
 
+import (
+	"fmt"
+	"strings"
+)
+
+// -------------------------------------------------------------------------------------------------
+// Traces
+// -------------------------------------------------------------------------------------------------
+
 // Trace is a stack-based logger which allows us to follow a simulation execution
 type Trace struct {
 	enabled bool
 
-	stack []*frame
+	stack []Frame
 }
 
-// curr returns a pointer to the current (topmost) frame
-func (t *Trace) curr() *frame {
+// curr returns a pointer to the current (topmost) Frame
+func (t *Trace) curr() *Frame {
 	if len(t.stack) <= 0 {
-		panic("attempt to look up current frame for empty stack")
+		panic("attempt to look up current Frame for empty stack")
 	}
 
-	return t.stack[len(t.stack)-1]
+	return &t.stack[len(t.stack)-1]
 }
 
-// New creates and returns a new Trace with a root frame initialized
+// New creates and returns a new Trace with a root Frame initialized
 func New() *Trace {
-	root := frame{header: "root"}
 	t := Trace{
-		stack: []*frame{
-			&root,
+		stack: []Frame{
+			Frame{
+				Header: "root",
+				Depth:  0,
+			},
 		},
 	}
 	return &t
@@ -37,24 +48,24 @@ func (t *Trace) Disable() {
 	t.enabled = false
 }
 
-// Push creates a new frame and adds it to the top of the stack
+// Push creates a new Frame and adds it to the top of the stack
 func (t *Trace) Push(header string, args ...any) {
 	if !t.enabled {
 		return
 	}
 
-	subframe := t.curr().subframe(header, args...)
-	t.stack = append(t.stack, subframe)
+	subFrame := t.curr().subFrame(header, args...)
+	t.stack = append(t.stack, subFrame)
 }
 
-// Pop removes the topmost frame from the trace and saves it
+// Pop removes the topmost Frame from the trace and saves it
 func (t *Trace) Pop() {
 	if !t.enabled {
 		return
 	}
 
 	if len(t.stack) <= 1 {
-		panic("attempt to pop root frame from trace stack")
+		panic("attempt to pop root Frame from trace stack")
 	}
 
 	t.stack = t.stack[:len(t.stack)-1]
@@ -76,4 +87,158 @@ func (t *Trace) Decision(msg string, args ...any) {
 	}
 
 	t.curr().emit(msg, args...)
+}
+
+// Walk recursively walks the [Frame] objects emitted by the [Trace] and calls the provided function
+// for each emitted event
+func (t *Trace) Walk(w Walker) {
+	if len(t.stack) == 0 {
+		panic("trace somehow has empty stack")
+	}
+
+	walk(w, t.stack[0])
+}
+
+// walk is the internal helper function for [Trace.Walk]
+func walk(w Walker, fr Frame) {
+	w.FrameStart(fr)
+	defer w.FrameEnd(fr)
+
+	for _, evt := range fr.hist {
+		switch evt.eventType {
+		case eventTypeMessage:
+			w.Message(fr, evt.message)
+		case eventTypeSubFrame:
+			walk(w, evt.subFrame)
+		default:
+			panic("unexpected event type: " + evt.eventType)
+		}
+	}
+}
+
+// String returns the [Trace] formatted as a barebones human-readable string
+func (t *Trace) String() string {
+	p := Printer{}
+	t.Walk(&p)
+	return p.Print()
+}
+
+// -------------------------------------------------------------------------------------------------
+// Walker
+// -------------------------------------------------------------------------------------------------
+
+// Walker defines the interface for a type that is able to recursively walk a [Trace] execution
+type Walker interface {
+	FrameStart(Frame)
+	Message(Frame Frame, message string)
+	FrameEnd(Frame)
+}
+
+// -------------------------------------------------------------------------------------------------
+// Printer
+// -------------------------------------------------------------------------------------------------
+
+// Printer is a specialized [Walker] which collects frames and messages as strings as it walks the
+// Trace execution and presents them in human-readable form
+type Printer struct {
+	messages []string
+}
+
+func (p *Printer) Add(s string) {
+	p.messages = append(p.messages, s)
+}
+
+func (p *Printer) Print() string {
+	return strings.Join(p.messages, "\n")
+}
+
+func (p *Printer) Indent(fr Frame) string {
+	return strings.Repeat("  ", fr.Depth)
+}
+
+func (p *Printer) FrameStart(fr Frame) {
+	p.Add(
+		fmt.Sprintf("%s[%s]", p.Indent(fr), fr.Header),
+	)
+}
+
+func (p *Printer) Message(fr Frame, msg string) {
+	p.Add(
+		fmt.Sprintf("%s  %s", p.Indent(fr), msg),
+	)
+}
+
+func (p *Printer) FrameEnd(fr Frame) {
+	p.Add(
+		fmt.Sprintf("%send", p.Indent(fr)),
+	)
+}
+
+// -------------------------------------------------------------------------------------------------
+// Frames
+// -------------------------------------------------------------------------------------------------
+
+// Frame represents a logical span of evaluation logic, i.e. "evaluating resource policies"
+type Frame struct {
+	Header string
+	Depth  int
+	hist   []event
+}
+
+func (f *Frame) emit(msg string, args ...any) {
+	next := event{
+		eventType: eventTypeMessage,
+		message:   format(msg, args...),
+	}
+	f.hist = append(f.hist, next)
+}
+
+func (f *Frame) subFrame(header string, args ...any) Frame {
+	subFrame := Frame{
+		Header: format(header, args...),
+		Depth:  f.Depth + 1,
+	}
+	next := event{
+		eventType: eventTypeSubFrame,
+		subFrame:  subFrame,
+	}
+	f.hist = append(f.hist, next)
+	return subFrame
+}
+
+// -------------------------------------------------------------------------------------------------
+// Events
+// -------------------------------------------------------------------------------------------------
+
+// eventType is an enum representing the different type of events we may emit
+type eventType string
+
+const (
+	eventTypeMessage  eventType = "EVT_TYPE_MESSAGE"
+	eventTypeSubFrame eventType = "EVT_TYPE_SUBFRAME"
+)
+
+// event represents an emitted event
+type event struct {
+	eventType eventType // always present
+
+	message  string // only present when eventType == eventTypeMessage
+	subFrame Frame  // only present when eventType == eventTypeSubFrame
+}
+
+// -------------------------------------------------------------------------------------------------
+// Formatting
+// -------------------------------------------------------------------------------------------------
+
+// format is a helper function for sprintf-style formatting of records
+//
+// This is factored out for two reasons:
+// - avoid allocations for arg-less formatting calls
+// - provide a single place for any formatting customizations
+func format(msg string, args ...any) string {
+	if len(args) == 0 {
+		return msg
+	}
+
+	return fmt.Sprintf(msg, args...)
 }
--- pkg/sim/trace/trace_test.go
+++ pkg/sim/trace/trace_test.go
@@ -1,18 +1,57 @@
 package trace
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/nsiow/yams/internal/testlib"
 )
 
 func TestTrace(t *testing.T) {
-	tests := []testlib.TestCase[func(*Trace), []*frame]{
+	type output struct {
+		frames []Frame
+		string []string
+	}
+
+	tests := []testlib.TestCase[func(*Trace), output]{
 		{
 			Name:  "empty_trace",
 			Input: func(t *Trace) {},
-			Want: []*frame{
-				{header: "root"},
+			Want: output{
+				frames: []Frame{
+					{
+						Header: "root",
+					},
+				},
+				string: []string{
+					"begin: root",
+					"end: root",
+				},
+			},
+		},
+		{
+			Name: "single_message",
+			Input: func(t *Trace) {
+				t.Observation("foo")
+			},
+			Want: output{
+				frames: []Frame{
+					{
+						Header: "root",
+						hist: []event{
+							{
+								eventType: eventTypeMessage,
+								message:   "foo",
+							},
+						},
+					},
+				},
+				string: []string{
+					"begin: root",
+					"  foo",
+					"end: root",
+				},
 			},
 		},
 		// {
@@ -20,18 +59,71 @@ func TestTrace(t *testing.T) {
 		// 	Input: func(t *Trace) {
 		// 		t.Observation("foo")
 		// 	},
-		// 	Want: []*frame{
-		// 		{
-		// 			header: "root",
-		// 			hist:   []event{},
+		// 	Want: output{
+		// 		frames: []*frame{
+		// 			{
+		// 				header: "root",
+		// 				hist: []event{
+		// 					{
+		// 						eventType: eventTypeMessage,
+		// 						message:   "foo",
+		// 					},
+		// 				},
+		// 			},
 		// 		},
+		// 		string: "",
 		// 	},
 		// },
 	}
 
-	testlib.RunTestSuite(t, tests, func(f func(*Trace)) ([]*frame, error) {
+	testlib.RunTestSuite(t, tests, func(f func(*Trace)) (output, error) {
 		trc := New()
+		trc.Enable()
 		f(trc)
-		return trc.stack, nil
+
+		pr := TestPrinter{}
+		trc.Walk(&pr)
+
+		return output{
+			frames: trc.stack,
+			string: strings.Split(pr.Print(), "\n"),
+		}, nil
 	})
+}
+
+// TestPrinter is a re-implementation of the [Printer] struct which allows us to more easily test
+// and confirm correct functionality without having to refactor tests if we decide to change the
+// user-facing formatting of the default [Printer]
+type TestPrinter struct {
+	messages []string
+}
+
+func (p *TestPrinter) Add(s string) {
+	p.messages = append(p.messages, s)
+}
+
+func (p *TestPrinter) Print() string {
+	return strings.Join(p.messages, "\n")
+}
+
+func (p *TestPrinter) Indent(fr Frame) string {
+	return strings.Repeat("  ", fr.Depth)
+}
+
+func (p *TestPrinter) FrameStart(fr Frame) {
+	p.Add(
+		fmt.Sprintf("%sbegin: %s", p.Indent(fr), fr.Header),
+	)
+}
+
+func (p *TestPrinter) Message(fr Frame, msg string) {
+	p.Add(
+		fmt.Sprintf("%s  %s", p.Indent(fr), msg),
+	)
+}
+
+func (p *TestPrinter) FrameEnd(fr Frame) {
+	p.Add(
+		fmt.Sprintf("%send: %s", p.Indent(fr), fr.Header),
+	)
 }
